b1f58d9cc12512cd640479a839cb37a551657b2e
Evan Krone <evankrone1@gmail.com>
2019-08-22 18:02:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,17 @@
 def nyc_pigeon_organizer(data)
  hash = { }
   data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+      if !hash[name]
+        hash[name] = { }
+      end
+      
+      if !hash[name][key]
+        hash[name][key] = [ ]
+      end
+      
+      hash[name][key]
+        
     
 end
